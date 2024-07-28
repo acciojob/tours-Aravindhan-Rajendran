9bc4a038951eb4b07e83faaa8c0952b106bff535
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-07-28 23:19:24 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import Tours from './Tours';
 import Loading from './Loading';
-import ../styles/App.css
+import '../styles/App.css';
 const App = () => {
   const [loading, setLoading] = useState(true);
   const [tours, setTours] = useState([]);
